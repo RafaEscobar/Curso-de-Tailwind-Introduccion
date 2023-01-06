555cd53c634e13d98032efdaa53e336b4864fd50
RafaEscobar <rafael.eg.code@gmail.com>
2023-01-05 20:41:42 -0600

    Avance, hasta grilla 100%

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,24 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Hola my love</h1>
+    
+    <div class="container">
+        <h1>Hola my love</h1>
+        <div class="grid grid-flow-col grid-rows-4 grid-cols-3">
+            <div class="bg-yellow-50">1</div>
+            <div class="bg-yellow-100">2</div>
+            <div class="bg-yellow-200">3</div>
+            <div class="bg-yellow-300">4</div>
+            <div class="bg-yellow-400">5</div>
+            <div class="bg-yellow-500">6</div>
+            <div class="bg-yellow-600">7</div>
+            <div class="bg-yellow-700">8</div>
+            <div class="bg-yellow-800">9</div>
+            <div class="bg-yellow-900">10</div>
+            <div class="bg-orange-400">11</div>
+            <div class="bg-orange-500">12</div>
+        </div>
+    </div>
+
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '14': 'repeat(14, minmax(0, 1fr))',
+
+        // Complex site-specific column configuration
+        'footer': '200px minmax(900px, 1fr) 100px',
+      }
+    },
   },
   plugins: [],
 }
